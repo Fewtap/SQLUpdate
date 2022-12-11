23b25ae64ff2e18d52f926b9f63a73b608abb015
fewtap9 <banansplit21@hotmail.com>
2022-12-11 05:00:32 -0300

    Can now update and insert new flights to the table with hash as the identifier

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import mysql.connector
 import FlightData
+from time import sleep
 
 mydb = mysql.connector.connect(
   host="lin-13041-7784-mysql-primary.servers.linodedb.net",
@@ -10,41 +11,36 @@ mydb = mysql.connector.connect(
   database="Departures"
 )
 
-def print_all(cursor):
-    sql = "SELECT * FROM Flights"
-    mycursor.execute(sql)
-    result = mycursor.fetchall()
-    for row in result:
-        print(row)
 
+while True:
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-url = "https://www.mit.gl/wp-content/themes/mitgl/webservice.php?type=Departures&icao=BGJN"
+    url = "https://www.mit.gl/wp-content/themes/mitgl/webservice.php?type=Departures&icao=BGJN"
 
-response = requests.get(url)
-data = response.json()
+    response = requests.get(url)
+    data = response.json()
 
-for row in data:
-    
+    for row in data:
+        
 
-    f = FlightData.Flight(
-        Rute=row['Rute'],
-        DepartureAirport = row['DepartureAirport'],
-        ArrivalAirport = row['ArrivalAirport'],
-        Planned = row['Planned'],
-        Estimated = row['Estimated'],
-        FlightHash = row['FlightHash'],
-        ArrivalICAO = row['ArrivalICAO'],
-        DepartureICAO = row['DepartureICAO'],
-        DeparturingRooms = None,
-        Status_kl = row['Status']['kl'],
-        Status_en = row['Status']['en'],
-        Status_da = row['Status']['da']
-    )
+        f = FlightData.Flight(
+            Rute=row['Rute'],
+            DepartureAirport = row['DepartureAirport'],
+            ArrivalAirport = row['ArrivalAirport'],
+            Planned = row['Planned'],
+            Estimated = row['Estimated'],
+            FlightHash = row['FlightHash'],
+            ArrivalICAO = row['ArrivalICAO'],
+            DepartureICAO = row['DepartureICAO'],
+            DeparturingRooms = None,
+            Status_kl = row['Status']['kl'],
+            Status_en = row['Status']['en'],
+            Status_da = row['Status']['da']
+        )
 
-    f.save(mydb)
+        f.save(mydb)
 
-
+    sleep(60)
 
 
